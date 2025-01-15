cfe02e8c6c39075e4ef428f0ccf5d0ffcd22703a
Slava Syurin <slava@syurin.com>
2025-01-15 14:08:44 +0100

    Add mains & battery feedbacks

--- feedbacks.js
+++ feedbacks.js
@@ -1,32 +1,57 @@
 const { combineRgb } = require('@companion-module/base')
+const _ = require('lodash')
 
 module.exports = async function (self) {
+	const upsListKeyMapping = {
+		name: 'id',
+		description: 'label',
+	};
+
+	let upsChoices = _.map(self.upsList, obj => _.mapKeys(obj, (value, key) => upsListKeyMapping[key] || key))
+
+	upsChoices = _.map(upsChoices, function (obj) {
+		obj.label = `${obj.id} - ${obj.label}`
+		return obj
+	})
+
 	self.setFeedbackDefinitions({
-		ChannelState: {
-			name: 'Example Feedback',
+		UPSOnMains: {
+			name: 'UPS on mains',
 			type: 'boolean',
-			label: 'Channel State',
+			label: 'UPS on mains',
 			defaultStyle: {
-				bgcolor: combineRgb(255, 0, 0),
+				bgcolor: combineRgb(0, 255, 0),
 				color: combineRgb(0, 0, 0),
 			},
-			options: [
-				{
-					id: 'num',
-					type: 'number',
-					label: 'Test',
-					default: 5,
-					min: 0,
-					max: 10,
-				},
-			],
+			options: [{
+				id: 'ups',
+				type: 'dropdown',
+				label: 'Select the UPS',
+				choices: upsChoices,
+				default: _.get(upsChoices, '0.id')
+			}],
 			callback: (feedback) => {
-				console.log('Hello world!', feedback.options.num)
-				if (feedback.options.num > 5) {
-					return true
-				} else {
-					return false
-				}
+				return _.get(self.deviceData, `${feedback.options.ups}.statusnum`) <= 2
+			},
+		},
+
+		UPSOnBattery: {
+			name: 'UPS on battery',
+			type: 'boolean',
+			label: 'UPS on battery',
+			defaultStyle: {
+				bgcolor: combineRgb(204, 0, 0),
+				color: combineRgb(255, 255, 255),
+			},
+			options: [{
+				id: 'ups',
+				type: 'dropdown',
+				label: 'Select the UPS',
+				choices: upsChoices,
+				default: _.get(upsChoices, '0.id')
+			}],
+			callback: (feedback) => {
+				return _.get(self.deviceData, `${feedback.options.ups}.statusnum`) >= 3
 			},
 		},
 	})
