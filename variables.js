26495a4f0eda5c825c822ec148cce591e7400a7f
Slava Syurin <slava@syurin.com>
2025-01-15 14:08:29 +0100

    Add dynamic variables

--- variables.js
+++ variables.js
@@ -1,7 +1,21 @@
+const variablesPerUps = [
+	{ id: 'status', name: 'The status of the #name# UPS entity' },
+	{ id: 'charge', name: 'The charge percentage of the #name# UPS entity' },
+]
+
 module.exports = function (self) {
-	self.setVariableDefinitions([
-		{ variableId: 'variable1', name: 'My first variable' },
-		{ variableId: 'variable2', name: 'My second variable' },
-		{ variableId: 'variable3', name: 'Another variable' },
-	])
+	let compiledVariableDefinitions = []
+
+	const upsIds = self.upsList.map((item) => item.name)
+
+	upsIds.forEach((id) =>
+		variablesPerUps.forEach((variable) =>
+			compiledVariableDefinitions.push({
+				variableId: `${id}_${variable.id}`,
+				name: variable.name.replace(/#name#/g, id),
+			}),
+		),
+	)
+
+	self.setVariableDefinitions(compiledVariableDefinitions)
 }
